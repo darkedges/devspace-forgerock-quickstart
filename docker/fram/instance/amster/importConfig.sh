e9932412edb33eb1e4b1bd26fd25cfe2fa0b8a3f
DarkEdges <nirving.darkedges@darkedges.com>
2024-05-05 06:54:51 +1000

    update

--- docker/fram/instance/amster/importConfig.sh
+++ docker/fram/instance/amster/importConfig.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
 export SERVER_PORT=${SERVER_PORT:-8080}
-export SERVER_URL=${SERVER_URL:-dfq-am.7f000001.nip.io}
+export SERVER_URL=${SERVER_URL:-am.7f000001.nip.io}
 export SERVER_SCHEME=${SERVER_SCHEME:-http}
 if [ -z "$SERVER_URL" ]; then
     HOSTNAME=`hostname -f` 
-    export AMSTER_URL="${SERVER_SCHEME}://${HOSTNAME}:${SERVER_PORT}/openam"
+    export AMSTER_URL="${SERVER_SCHEME}://${HOSTNAME}:${SERVER_PORT}/am"
 else
-    export AMSTER_URL="${SERVER_SCHEME}://${SERVER_URL}:${SERVER_PORT}/openam"
+    export AMSTER_URL="${SERVER_SCHEME}://${SERVER_URL}:${SERVER_PORT}/am"
 fi
 /opt/amster/amster /opt/amster/config/importConfig.amster
